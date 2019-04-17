c1a65828af98fb65432a965b1a90a423a35dfddd
Moceph71 <moceph71@comcast.net>
2019-04-17 10:57:08 -0500

    added q_learning and SARSA

--- src/functions.py
+++ src/functions.py
@@ -14,6 +14,17 @@ def q_learning(world, agent, qtable, action, next_action, learning_rate, discoun
     if isinstance(qtable[get_current_state(world, agent)], str):
         return
 
+    else:
+        state = get_current_state(world, agent)
+        assert action in ['north', 'south', 'east', 'west']
+        next_state = get_current_state(world, agent.pretend_move(action))
+        x, y = next_state[:2]
+        neighbors = world.get_neighbors(x, y)
+        dir = max(get_max_neighbors(neighbors, state, qtable))
+        r = world.get_reward(*agent.get_position(), agent.is_holding_block())
+        qtable[state][action] = (1 - learning_rate) * qtable[state][action] + (r + discount_rate * qtable[next_state][dir])
+       
+
     # not correct, just for testing
     qtable[get_current_state(world, agent)][action] = \
             world.get_reward(*agent.get_position(), agent.is_holding_block())
@@ -23,6 +34,17 @@ def SARSA(world, agent, qtable, action, next_action, learning_rate, discount_rat
     # value for it
     if isinstance(qtable[get_current_state(world, agent)], str):
         return
+
+    else:
+        assert action in ['north', 'south', 'east', 'west']
+        assert next_action in ['north', 'south', 'east', 'west']
+        state = get_current_state(world, agent)        
+        next_state = get_current_state(world, agent.pretend_move(action))
+        r = world.get_reward(*agent.get_position(), agent.is_holding_block())
+        qtable[state][action] = qtable[state][action] + learning_rate * ( r + qtable[next_state][dir] - qtable[state][action])
+    
+
+
 
     # not correct, just for testing
     qtable[get_current_state(world, agent)][action] = \
